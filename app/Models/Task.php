d98c840eb28cd9d9c860eeb2b3e23ea29b2c81b4
Alik <125959412+Turpa22@users.noreply.github.com>
2026-01-29 00:15:34 +0500

    Add files via upload

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -1,1 +1,27 @@
+<?php
 
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    protected $fillable = [
+        'title',
+        'description',
+        'status',
+        'status_text'
+    ];
+    public function getStatusTextAttribute()
+    {
+        $statuses = [
+            1 => 'новая',
+            2 => 'в работе',
+            3 => 'исполнена',
+            4 => 'отклонена',
+            5 => 'отменена'
+        ];
+        
+        return $statuses[$this->status] ?? 'неизвестный статус';
+    }
+}
