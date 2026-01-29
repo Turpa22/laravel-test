bdd59f57e196f048fb331c0d41cc4d4ad4b76edc
Alik <125959412+Turpa22@users.noreply.github.com>
2026-01-29 13:28:54 +0500

    Add files via upload

--- routes/api.php
+++ routes/api.php
@@ -1,1 +1,7 @@
+<?php
 
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
+
+Route::apiResource('tasks', TaskController::class);
+Route::get('tasks-table', [TaskController::class, 'table']);
